b0af9df43948e768beef3984dfe71819ec3f80a4
Meghraj Giri <meghrajgiri56@gmail.com>
2022-02-04 21:00:05 +0545

    mobile menu issue solved

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
-import React, { lazy, Suspense } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React, { lazy, Suspense } from "react"
+import { Switch, Route, Redirect } from "react-router-dom"
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
 
-import Navigation from "./components/navigation/navigation";
-import Footer from "./components/footer/footer";
-import Spinner from "./components/spinner/spinner";
-import ErrorBoundary from "./components/error-boundary/error-boundary";
+import Footer from "./components/footer/footer"
+import Spinner from "./components/spinner/spinner"
+import ErrorBoundary from "./components/error-boundary/error-boundary"
 
-import { selectCurrentUser } from "./redux/user/user.selectors";
-import { setCurrentUser } from "./redux/user/user.action";
+import { selectCurrentUser } from "./redux/user/user.selectors"
+import { setCurrentUser } from "./redux/user/user.action"
 
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
-import { GlobalStyle } from "./global-styles";
+import { GlobalStyle } from "./global-styles"
+import NavigationBar from "./components/navigation/NavigationBar"
 
-const Home = lazy(() => import("./pages/home/home"));
-const Shop = lazy(() => import("./pages/shop/shop"));
-const SignIn = lazy(() => import("./components/sign-in/sign-in"));
-const SignUp = lazy(() => import("./components/sign-up/sign-up"));
-const Contact = lazy(() => import("./pages/contact/contact"));
-const CheckoutPage = lazy(() => import("./pages/checkout/checkout"));
+const Home = lazy(() => import("./pages/home/home"))
+const Shop = lazy(() => import("./pages/shop/shop"))
+const SignIn = lazy(() => import("./components/sign-in/sign-in"))
+const SignUp = lazy(() => import("./components/sign-up/sign-up"))
+const Contact = lazy(() => import("./pages/contact/contact"))
+const CheckoutPage = lazy(() => import("./pages/checkout/checkout"))
 
 class App extends React.Component {
-  unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser } = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+        const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot((snapShot) => {
           setCurrentUser({
             id: snapShot.id,
             ...snapShot.data(),
-          });
-        });
+          })
+        })
       } else {
-        setCurrentUser(userAuth);
+        setCurrentUser(userAuth)
       }
-    });
+    })
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth()
   }
 
   render() {
     return (
       <div className="flex-1 top-0">
-        <Navigation />
+        <NavigationBar />
         <Switch>
           <ErrorBoundary>
             <Suspense fallback={<Spinner />}>
@@ -77,16 +77,16 @@ class App extends React.Component {
         <Footer />
         <GlobalStyle />
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-});
+})
 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
